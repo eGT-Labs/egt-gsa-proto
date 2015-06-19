4b64ecd1a5a9f7c343d0bb83fe1d81632928880c
Eric Hanson <j.eric.hanson@gmail.com>
2015-06-19 08:43:36 -0400

    protecting against out-of-order API responses.

--- client/views/label-search/label-search.controller.js
+++ client/views/label-search/label-search.controller.js
@@ -7,41 +7,66 @@ angular.module('egtGsaProto')
     angular.extend(vm, {
       query: {
         fulltext: '',
-        selectedFacets: {},
+        selectedFacets: { //data structure for which facets options are selected
+          generic_name: [],
+          brand_name: [],
+          manufacturer_name: [],
+          product_type: [],
+          route: [],
+          substance_name: [],
+          pharm_class_cs: [],
+          pharm_class_moa: [],
+          pharm_class_epc: []
+        },
         pageSize: 10,
         pageNum: 1
       },
       resp: null,
-      respText: ''
+      facets: { //data structure for which facets are available
+        generic_name: null,
+        brand_name: null,
+        manufacturer_name: null,
+        product_type: null,
+        route: null,
+        substance_name: null,
+        pharm_class_cs: null,
+        pharm_class_moa: null,
+        pharm_class_epc: null
 
+      },
+      respText: ''
     });
 
-    vm.pageCount = function() {
-      if (vm.resp) {
-        var count = vm.resp.data.meta.results.total;
-        return Math.ceil(count / vm.query.pageSize);
-      } else {
-        return 0;
-      }
-    };
 
+    var latestQuery;
 
     vm.executeQuery = function (resetPageNum) {
+
+      var thisQuery = Date.now();
+      latestQuery = thisQuery;
+
       if (resetPageNum) {
         vm.query.pageNum = 1;
       }
 
+
+
       LabelFactory.runQuery({
-        search: vm.query.fulltext,
+        search: (vm.query.fulltext || '""') + ' AND _exists_:openfda.spl_id', //TODO this is a hack
         limit: vm.query.pageSize,
         skip: (vm.query.pageNum - 1) * vm.query.pageSize
       }).then(
         function (resp) {
-          vm.resp = resp;
-          vm.respText = angular.toJson(resp,true);
+          if (latestQuery === thisQuery) {
+            vm.resp = resp;
+            vm.respText = angular.toJson(resp, true);
+          } else {
+            console.log('ignoring outdated query');
+          }
         }
       )
     };
 
+    vm.executeQuery(true);
 
   });
