4ac025b72c61435fd29f1a3bd7577591b35c8b45
Manasa.Pandurangi <Manasa.Pandurangi@eglobaltech.com>
2015-06-19 09:26:01 -0400

    detail page

--- client/views/label/label.controller.js
+++ client/views/label/label.controller.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('egtGsaProto')
-  .controller('LabelCtrl', function ($routeParams, LabelFactory) {
+  .controller('LabelCtrl', function ($scope,$routeParams, LabelFactory) {
     var vm = this;
 
     vm.status = 'loading';
     vm.id = $routeParams.id;
 
 
+    $scope.isArray = angular.isArray;
+    
     LabelFactory.load(vm.id).then(
       function (label) {
         vm.status = 'success';
