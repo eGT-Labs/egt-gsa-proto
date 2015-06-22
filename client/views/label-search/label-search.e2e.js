43f30f99bdefef11daa33a3ed413ec15cf31dc0f
Zach Rowe <zachary.rowe@eglobaltech.com>
2015-06-22 18:07:48 -0400

    add test for error page

--- client/views/label-search/label-search.e2e.js
+++ client/views/label-search/label-search.e2e.js
@@ -8,4 +8,15 @@ describe('label-search route', function () {
     expect(element.all(by.css('body > div.container.ng-scope > div.row.ng-scope > div.col-sm-4 > accordion > div > div:nth-child(1) > div > div.panel-heading > h4 > a > div')).first().getText()).toBe('Brand Name (100)');
   });
 
+  it('should display an error with no results', function () {
+    browser.get('/label-search');
+    var search_box = element(by.name('fulltext'));
+    var search = 'foobarbaz';
+
+    search_box.sendKeys(search);
+    search_box.submit();
+
+    expect(element.all(by.css('#error')).first().getText()).toContain('No matches found!');
+  });
+
 });
