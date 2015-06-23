6595749174e6c1677102b02b9eeb358d8dfe6d9d
Zach Rowe <zachary.rowe@eglobaltech.com>
2015-06-23 12:52:02 -0400

    add test for basic label info

--- client/views/label/label.e2e.js
+++ client/views/label/label.e2e.js
@@ -9,6 +9,8 @@ describe('label route', function () {
   it('should have basic content for the drug Claritin', function () {
 
     browser.get('/label/e225144b-e9d6-439f-8785-01bdb350a080');
+    expect(element.all(by.css('body > div.container.ng-scope > div > div > div.row > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)')).first().getText()).toContain('PSEUDOEPHEDRINE SULFATE');
+  });
 
 
 
