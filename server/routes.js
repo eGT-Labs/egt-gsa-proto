952228bafd9ed323225985b02b349e380b7b51fb
Eric Hanson <j.eric.hanson@gmail.com>
2015-06-24 11:24:26 -0400

    caching adverse event queries server side to decrease query load on the OpenFDA service

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,15 @@
 var config = require('./config/environment');
 var proxy = require('express-http-proxy');
 var url = require('url');
+var superagent = require('superagent');
+var Promise = require("bluebird");
+var LRU = require("lru-cache");
+
+
+var eventCountCache = LRU({
+  max: 1000000,
+  maxAge: 1000 * 60 * 60 * 24 //one day
+});
 
 
 module.exports = function (app) {
@@ -12,11 +21,9 @@ module.exports = function (app) {
     decorateRequest: function (req) {
       //IF the server has been configured with an OpenFDA API key, attach the key to each proxied request.
       if (config.openFdaApiKey) {
-        var keys = config.openFdaApiKey.split(',');
-        var key = keys[Math.floor(keys.length * Math.random())]; //randomly choose a key from the keyset
         var pathObj = url.parse(req.path, true);
         delete pathObj.search;
-        pathObj.query['api_key'] = key;
+        pathObj.query['api_key'] = config.openFdaApiKey;
         req.path = url.format(pathObj);
       }
       return req
@@ -29,6 +36,56 @@ module.exports = function (app) {
       callback(null, data);
     }
   }));
+
+  app.route('/api/countEvents')
+    .get(function (req, res) {
+      var type = req.query.type;
+      var value = req.query.value;
+      var otherType = req.query.otherType;
+
+      var elasticSeearchQuery = type + ':"' + value + '"+AND+_exists_:(' + otherType + ')';
+      var countPromise = new Promise(function (resolve, reject) {
+        var cacheValue = eventCountCache.get(elasticSeearchQuery);
+
+        if (cacheValue) {
+          resolve(cacheValue);
+        } else {
+          var queryWithKey = elasticSeearchQuery;
+          if (config.openFdaApiKey) {
+            queryWithKey += '&api_key=' + config.openFdaApiKey;
+          }
+          superagent.get('http://api.fda.gov/drug/event.json?limit=1&search=' + queryWithKey)
+            .end(function (err, res) {
+              if (err) {
+                if (err.status == 404 || err.status == 400) {
+                  eventCountCache.set(elasticSeearchQuery, 0);
+                  resolve(0);
+                } else {
+                  reject(err.status);
+                }
+                console.log(err.status)
+              } else {
+                var count = res.body.meta.results.total
+                eventCountCache.set(elasticSeearchQuery, count)
+                resolve(count);
+              }
+            });
+        }
+      });
+
+      countPromise.then(
+        function (count) {
+          res
+            .set('cache-control', 'max-age=86400')
+            .send({count: count});
+        }, function (errStatus) {
+          res
+            .status(errStatus)
+            .end();
+        });
+    }
+  )
+  ;
 
   app.route('/:url(api|app|bower_components|assets)/*')
     .get(function (req, res) {
@@ -43,4 +100,5 @@ module.exports = function (app) {
       );
     });
 
-};
+}
+;
